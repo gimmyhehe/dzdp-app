64843c51efe7d1f0bd40a3ab85db9b1e72e27b10
gimmy <975402925@qq.com>
2019-01-02 23:30:13 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            initDone: false
+        }
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.props.children
+                }
+            </div>
+        )
+    }
+}
+
+// -------------------redux react 绑定--------------------
+
+export default App
---
+++ src/router/routeMap.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Router, Route, IndexRoute } from 'react-router'
+
+import App from '../pages'
+import Home from '../pages/Home'
+import City from '../pages/City'
+// import User from '../pages/User'
+// import Search from '../pages/Search'
+// import Detail from '../pages/Detail'
+// import NotFound from '../pages/404'
+
+// 如果是大型项目，router部分就需要做更加复杂的配置
+// 参见 https://github.com/reactjs/react-router/tree/master/examples/huge-apps
+
+class RouterMap extends React.Component {
+    render() {
+        return (
+            <Router history={this.props.history}>
+                <Route path='/' component={App}>
+                    <IndexRoute component={Home}/>
+                    <Route path='/city' component={City}/>
+                </Route>
+            </Router>
+        )
+    }
+}
+
+export default RouterMap
